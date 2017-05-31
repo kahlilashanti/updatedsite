cf5d89fe02d8272a0ba144e5f99236158db7445d
Kahlil Ashanti <kahlilashanti2@Kahlils-MacBook-Air.local>
2017-05-30 20:47:55 -0700

    updated bio section

--- javascript/file.js
+++ javascript/file.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   Typed.new('.element', {
-    strings: ["Actor", "Hacker", "Speaker", "Entrepreneur","Writer"],
+    strings: ["Actor", "Web Developer", "Speaker", "Entrepreneur","Writer"],
     typeSpeed: 100,
     loop: true,
     showCursor: true
